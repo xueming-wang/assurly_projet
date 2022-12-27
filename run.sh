7cb2fa002570d650be54200aaca33bba6bcfd31a
xueming-wang <wxueming0614@hotmail.com>
2022-12-27 01:47:01 +0100

    ok

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ docker-compose up -d --build
 echo "Applying migrations..."
 sleep 5
 
-docker exec -it assurly_project_backend_1 python manage.py makemigrations assurly
-docker exec -it assurly_project_backend_1 python manage.py migrate
+docker exec -it assurly_project_backend python manage.py makemigrations assurly
+docker exec -it assurly_project_backend python manage.py migrate
 
 echo "Migrations applied"
